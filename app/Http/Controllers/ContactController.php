c0ceabe514edee5f37deec0432e03371b84c3036
tanzim721 <53827851+tanzim721@users.noreply.github.com>
2024-10-26 11:19:54 +0600

    Conatct mail

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\ContactMail;
+
+class ContactController extends Controller
+{
+    public function showForm()
+    {
+        return "Hello World";
+    }
+    public function submitForm(Request $request)
+    {
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255'],
+            'subject' => ['required', 'string', 'max:255'],
+            'message' => ['required', 'string'],
+        ]);
+        Mail::to('tanzimulislam799@gmail.com')->send(new ContactMail($request->name, $request->email, $request->subject, $request->message));
+    }
+}
