bc25dde6a71f0790af6c709e01a52f05ed3dfa3b
tanzim721 <53827851+tanzim721@users.noreply.github.com>
2024-11-17 22:17:19 +0600

    admin panel modify

---
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class DashboardController extends Controller
+{
+    public function index()
+    {
+        $users = User::all();
+        return view('dashboard', compact('users'));
+    }
+}
--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -1,20 +1,18 @@
 <nav id="sidebar">
     <!-- Sidebar Header-->
     <div class="sidebar-header d-flex align-items-center">
-        <div class="avatar"><img src="{{ asset('backend/img/avatar-6.jpg') }}" alt="..."
-                class="img-fluid rounded-circle"></div>
+        <div class="avatar"><img src="{{ asset('backend/img/avatar-6.jpg') }}" alt="..." class="img-fluid rounded-circle"></div>
         <div class="title">
-            <h1 class="h5">Mark Stephen</h1>
-            <p>Web Designer</p>
+            <h1 class="h5">{{ strtoupper(Auth::user()->name) }}</h1>
+            <p>Admin</p>
         </div>
     </div>
-    <!-- Sidebar Navidation Menus--><span class="heading">Main</span>
     <ul class="list-unstyled">
-        <li class="{{Request::is('admin/dashboard*') ? 'active' : ''}}"><a href="{{ route('admin.dashboard') }}"> <i class="icon-home"></i>Home </a></li>
+        <li class="{{Request::is('dashboard*') ? 'active' : ''}}"><a href="{{ route('dashboard') }}"> <i class="icon-home"></i>Home </a></li>
 
-        <li class="{{ (Request::is('category/view*') || Request::is('category/edit*')) ? 'active' : ''}}"><a href="{{ route('category.view') }}"> <i class="fa fa-list-alt"></i>Category </a></li>
+        {{-- <li class="{{ (Request::is('category/view*') || Request::is('category/edit*')) ? 'active' : ''}}"><a href="{{ route('category.view') }}"> <i class="fa fa-list-alt"></i>Category </a></li>
 
-        <li class="{{ (Request::is('product/view*') || Request::is('product/add*') || Request::is('product/edit*')) ? 'active' : ''}}"><a href="{{ route('admin.product.view') }}"> <i class="fa fa-list-alt"></i>Product </a></li>
+        <li class="{{ (Request::is('product/view*') || Request::is('product/add*') || Request::is('product/edit*')) ? 'active' : ''}}"><a href="{{ route('admin.product.view') }}"> <i class="fa fa-list-alt"></i>Product </a></li> --}}
 
         {{-- <li class="{{ (Request::is('product/view*') || Request::is('product/add*') || Request::is('product/edit*')) ? 'active' : ''}}"><a href="#exampledropdownDropdown" aria-expanded="false" data-toggle="collapse"> <i class="icon-windows"></i>Product </a>
             <ul id="exampledropdownDropdown" class="collapse list-unstyled">
